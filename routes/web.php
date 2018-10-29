228a5ebc12fefa73ef5bf873bb0fcc8577ecab30
MuhammadAbran <abran7815@gmail.com>
2018-10-30 01:28:50 +0700

    a

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('profile', 'ProfileController@index')->name('profile.page');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
